44e943518d96c9831e8228e10f53150800ca1fdc
zhangzhi666 <837142311@qq.com>
2017-07-17 12:43:37 +0800

    初始环境测试成功

--- app/src/main/java/com/hc62u/zz/hc62position/MainActivity.java
+++ app/src/main/java/com/hc62u/zz/hc62position/MainActivity.java
@@ -94,8 +94,8 @@ public class MainActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 }
